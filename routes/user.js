2854f7719dbb918218044d9005c18a2c233989b5
cacheUp <bradb26@gmail.com>
2019-06-28 16:36:00 +0300

    added isAuth and isAdmin

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const { userById } = require("../controllers/user");
-const { requireSignIn } = require("../controllers/auth");
+const { requireSignIn, isAdmin, isAuth } = require("../controllers/auth");
 
-router.param("userId", userById);
-router.get("/secret/:userId", requireSignIn, (req, res) => {
+router.get("/secret/:userId", requireSignIn, isAuth, isAdmin, (req, res) => {
   res.json({ user: req.profile });
 });
+
+router.param("userId", userById);
 
 module.exports = router;
