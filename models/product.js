bfadcf5d15e86f0e14c8b2d63cdb4461f84945b4
cacheUp <bradb26@gmail.com>
2019-06-29 17:31:04 +0300

    more product model

--- models/product.js
+++ models/product.js
@@ -10,6 +10,23 @@ const productSchema = new mongoose.Schema(
       trim: true,
       required: true,
       maxlength: 32
+    },
+    description: {
+      type: String,
+      trim: true,
+      required: true,
+      maxlength: 2000
+    },
+    price: {
+      type: Number,
+      trim: true,
+      required: true,
+      maxlength: 32
+    },
+    category: {
+      type: ObjectId,
+      ref: "Category",
+      required: true
     }
   },
   { timestamps: true }
