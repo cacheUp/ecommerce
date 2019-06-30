ca04c8492dcab0b742e7770640321ef67f6ab5d0
cacheUp <bradb26@gmail.com>
2019-06-30 20:00:28 +0300

    update product, working and tested

--- routes/product.js
+++ routes/product.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const { requireSignIn, isAdmin, isAuth } = require("../controllers/auth");
-const { create, productById, read, remove } = require("../controllers/product");
+const {
+  create,
+  productById,
+  read,
+  remove,
+  update
+} = require("../controllers/product");
 const { userById } = require("../controllers/user");
 
 router.get("/product/:productId", read);
@@ -12,8 +18,15 @@ router.delete(
   requireSignIn,
   isAdmin,
   isAuth,
-
   remove
+);
+
+router.put(
+  "/product/:productId/:userId",
+  requireSignIn,
+  isAdmin,
+  isAuth,
+  update
 );
 router.param("userId", userById);
 router.param("productId", productById);
