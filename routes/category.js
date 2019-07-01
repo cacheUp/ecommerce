be6de5c854b0ed8d3d3ad43723bd3b22ff2b5cf2
cacheUp <bradb26@gmail.com>
2019-07-01 20:10:14 +0300

    categoryById, read in category controllers

--- routes/category.js
+++ routes/category.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const { requireSignIn, isAdmin, isAuth } = require("../controllers/auth");
-const { create } = require("../controllers/category");
+const { create, categoryById, read } = require("../controllers/category");
 const { userById } = require("../controllers/user");
 
+router.get("/category/:categoryId", read);
 router.post("/category/create/:userId", requireSignIn, isAdmin, isAuth, create);
-router.param("userId", userById);
 
+router.param("userId", userById);
+router.param("categoryId", categoryById);
 module.exports = router;
