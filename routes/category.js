c944f22275d70eecdea5680102aaf60e15be021d
cacheUp <bradb26@gmail.com>
2019-07-01 20:57:57 +0300

    updated product model

--- routes/category.js
+++ routes/category.js
@@ -1,11 +1,33 @@
 const express = require("express");
 const router = express.Router();
 const { requireSignIn, isAdmin, isAuth } = require("../controllers/auth");
-const { create, categoryById, read } = require("../controllers/category");
+const {
+  create,
+  categoryById,
+  read,
+  remove,
+  list,
+  update
+} = require("../controllers/category");
 const { userById } = require("../controllers/user");
 
 router.get("/category/:categoryId", read);
 router.post("/category/create/:userId", requireSignIn, isAdmin, isAuth, create);
+router.put(
+  "/category/:categoryId/:userId",
+  requireSignIn,
+  isAdmin,
+  isAuth,
+  update
+);
+router.delete(
+  "/category/:categoryId/:userId",
+  requireSignIn,
+  isAdmin,
+  isAuth,
+  remove
+);
+router.get("/categories", list);
 
 router.param("userId", userById);
 router.param("categoryId", categoryById);
