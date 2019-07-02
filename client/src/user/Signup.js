1d8ff740d91d8463f6a8264fd94a16bd11325903
cacheUp <bradb26@gmail.com>
2019-07-02 17:02:44 +0300

    add Layout around signin and signup

--- client/src/user/Signup.js
+++ client/src/user/Signup.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Layout from "../core/Layout";
 
 const Signup = () => {
-  return <div>Sign up</div>;
+  return (
+    <Layout title="Home Page" description="Node React E-Commerce App">
+      ...
+    </Layout>
+  );
 };
 
 export default Signup;
