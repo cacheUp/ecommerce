ac5231037203810fee2712edf69c619e6a07eeb7
cacheUp <bradb26@gmail.com>
2019-07-02 16:52:58 +0300

    Menu and Layout

--- client/src/core/Home.js
+++ client/src/core/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
-import Layout from "../core/Layout";
+import Layout from "./Layout";
 
-const Signin = () => {
+const Home = () => {
   return (
     <Layout title="Home Page" description="Node React E-Commerce App">
       ...
@@ -9,4 +9,4 @@ const Signin = () => {
   );
 };
 
-export default Signin;
+export default Home;
