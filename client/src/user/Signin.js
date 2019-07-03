1dc99cb8839283d920ccca31c692aed5d33d212a
cacheUp <bradb26@gmail.com>
2019-07-03 17:31:13 +0300

    sign in component built

--- client/src/user/Signin.js
+++ client/src/user/Signin.js
@@ -1,10 +1,97 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from "../core/Layout";
 
+import { Link, Redirect } from "react-router-dom";
+import { signin } from "../auth";
+
 const Signin = () => {
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+    error: "",
+    success: false,
+    loading: false,
+    redirectToReferrer: false
+  });
+
+  const { email, password, error, loading, redirectToReferrer } = values;
+
+  const handleChange = name => event => {
+    setValues({ ...values, error: false, [name]: event.target.value });
+  };
+
+  const clickSubmit = event => {
+    event.preventDefault();
+    setValues({ ...values, error: false, loading: true });
+    signin({ email, password }).then(data => {
+      if (data.error) {
+        setValues({ ...values, error: data.error, loading: false });
+      } else {
+        setValues({
+          ...values,
+          redirectToReferrer: true
+        });
+      }
+    });
+  };
+  const signInForm = () => (
+    <form>
+      <div className="form-group">
+        <label className="text-muted">Email</label>
+        <input
+          onChange={handleChange("email")}
+          type="email"
+          className="form-control"
+          value={email}
+        />
+      </div>
+      <div className="form-group">
+        <label className="text-muted">Password</label>
+        <input
+          onChange={handleChange("password")}
+          type="password"
+          className="form-control"
+          value={password}
+        />
+      </div>
+      <button onClick={clickSubmit} className="btn btn-primary">
+        Submit
+      </button>
+    </form>
+  );
+
+  const showError = () => (
+    <div
+      className="alert alert-danger"
+      style={{ display: error ? "" : "none" }}
+    >
+      {error}
+    </div>
+  );
+
+  const showLoading = () =>
+    loading && (
+      <div className="alert alert-info">
+        <h2>Loading...</h2>
+      </div>
+    );
+
+  const redirectUser = () => {
+    if (redirectToReferrer) {
+      return <Redirect to="/" />;
+    }
+  };
+
   return (
-    <Layout title="Home Page" description="Node React E-Commerce App">
-      ...
+    <Layout
+      title="Home Page"
+      description="Node React E-Commerce App"
+      className="container col-md-8 offset-md-2"
+    >
+      {showError()}
+      {showLoading()}
+      {redirectUser()}
+      {signInForm()}
     </Layout>
   );
 };
