186d18f5ff63f4e3bbffab019fbb47cbd7fe9efe
cacheUp <bradb26@gmail.com>
2019-07-04 17:40:27 +0300

    saved token to local storage on sign in

--- client/src/user/Signin.js
+++ client/src/user/Signin.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Layout from "../core/Layout";
 
 import { Link, Redirect } from "react-router-dom";
-import { signin } from "../auth";
+import { signin, authenticate } from "../auth";
 
 const Signin = () => {
   const [values, setValues] = useState({
@@ -27,9 +27,11 @@ const Signin = () => {
       if (data.error) {
         setValues({ ...values, error: data.error, loading: false });
       } else {
-        setValues({
-          ...values,
-          redirectToReferrer: true
+        authenticate(data, () => {
+          setValues({
+            ...values,
+            redirectToReferrer: true
+          });
         });
       }
     });
