4e5522fe7a0f355e4b46e41b74e9ce4006683907
cacheUp <bradb26@gmail.com>
2019-07-06 13:04:02 +0300

    added category api call

--- client/src/admin/AddCategory.js
+++ client/src/admin/AddCategory.js
@@ -2,11 +2,17 @@ import React, { useState } from "react";
 import Layout from "../core/Layout";
 import { isAuthenticated } from "../auth";
 import { Link } from "react-router-dom";
+import { addCategory } from "./apiAdmin";
 
 const AddCategory = () => {
   const [name, setName] = useState("");
   const [error, setError] = useState(false);
   const [success, setSuccess] = useState(false);
+
+  const {
+    user: { _id },
+    token
+  } = isAuthenticated();
 
   const handleChange = e => {
     setError("");
@@ -16,9 +22,16 @@ const AddCategory = () => {
     e.preventDefault();
     setError("");
     setSuccess(false);
+    addCategory(_id, token, { name }).then(data => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setError("");
+        setSuccess(true);
+      }
+    });
   };
 
-  const { user, token } = isAuthenticated();
   const newCategoryForm = () => (
     <form>
       <div className="form-group">
@@ -34,6 +47,17 @@ const AddCategory = () => {
       <button className="btn btn-outline-primary">Create a Category</button>
     </form>
   );
+
+  const showSuccess = () => {
+    if (success) {
+      return <h3 className="text-success">{name} is created</h3>;
+    }
+  };
+  const showError = () => {
+    if (error) {
+      return <h3 className="text-danger">{name} should be unique</h3>;
+    }
+  };
 
   return (
     <Layout title="Add a new category" description={`Hello ${name}`}>
