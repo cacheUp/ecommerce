926e61eaa0d30cc32bf6b0c552e16b6b95e7ebe1
cacheUp <bradb26@gmail.com>
2019-07-08 17:39:11 +0300

    product card

--- client/src/core/Home.js
+++ client/src/core/Home.js
@@ -1,10 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Layout from "./Layout";
+import { getProducts } from "./apiCore";
+import Card from "./Card";
 
 const Home = () => {
+  const [productsBySell, setProductsBySell] = useState([]);
+  const [productsByArrival, setProductsByArrival] = useState([]);
+  const [error, setError] = useState(false);
+
+  const loadProductsBySell = () => {
+    getProducts("sold").then(data => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setProductsBySell(data);
+      }
+    });
+  };
+
+  const loadProductsByArrival = () => {
+    getProducts("createdAt").then(data => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setProductsByArrival(data);
+      }
+    });
+  };
+
+  useEffect(() => {
+    loadProductsByArrival();
+    loadProductsBySell();
+  }, []);
+
   return (
     <Layout title="Home Page" description="Node React E-Commerce App">
-      ...
+      <h2 className="mb-4"> Best Sellers</h2>
+      {productsBySell.map((item, index) => (
+        <Card key={index} product={item} />
+      ))}
+
+      <h2 className="mb-4"> New Arrivals </h2>
+      {productsByArrival.map((item, index) => (
+        <Card key={index} product={item} />
+      ))}
     </Layout>
   );
 };
