8509e687b0519565e5071d5a76e5025f6cb28c61
cacheUp <bradb26@gmail.com>
2019-07-11 12:42:12 +0300

    checkbox and shop, added prices array, RadioBox init

--- client/src/core/Shop.js
+++ client/src/core/Shop.js
@@ -3,8 +3,12 @@ import Layout from "./Layout";
 import Card from "./Card";
 import { getCategories } from "./apiCore";
 import Checkbox from "./Checkbox";
+import { prices } from "./fixedPrices";
 
 const Shop = () => {
+  const [myFilters, setMyFilters] = useState({
+    filters: { category: [], price: [] }
+  });
   const [categories, setCategories] = useState([]);
   const [error, setError] = useState(false);
 
@@ -23,16 +27,25 @@ const Shop = () => {
     init();
   }, []);
 
+  const handleFilters = (filters, filterBy) => {
+    const newFilters = { ...myFilters };
+    newFilters.filters[filterBy] = filters;
+    setMyFilters(newFilters);
+  };
+
   return (
     <Layout title="Shop" className="container-fluid">
       <div className="row">
         <div className="col-4">
           <h4> Filter by Categories</h4>
           <ul>
-            <Checkbox categories={categories} />
+            <Checkbox
+              categories={categories}
+              handleFilters={filters => handleFilters(filters, "category")}
+            />
           </ul>
         </div>
-        <div className="col-8">right</div>
+        <div className="col-8">{JSON.stringify(myFilters)}</div>
       </div>
     </Layout>
   );
