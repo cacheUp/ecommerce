6fca0eaf31b573935d62ed5a7efccc5299abb42b
cacheUp <bradb26@gmail.com>
2019-07-19 10:20:52 +0300

    added Checkout

--- client/src/core/Cart.js
+++ client/src/core/Cart.js
@@ -3,6 +3,7 @@ import Layout from "./Layout";
 import { getCart } from "./cartHelpers";
 import Card from "./Card";
 import { Link } from "react-router-dom";
+import Checkout from "./Checkout";
 
 const Cart = () => {
   const [items, setItems] = useState([]);
@@ -51,7 +52,9 @@ const Cart = () => {
           {items.length > 0 ? showItems(items) : noItemsMessage()}
         </div>
         <div className="col-6">
-          <p>show checkout options/shipping address/total/update quantity</p>
+          <h2 className="mb-4"> Your cart summary</h2>
+          <hr />
+          <Checkout products={items} />
         </div>
       </div>
     </Layout>
