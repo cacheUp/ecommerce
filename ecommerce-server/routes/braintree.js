ea3e5323805c75f4dc62ecc89672a5a4b0566974
cacheUp <bradb26@gmail.com>
2019-07-19 10:46:19 +0300

    generate braintree token

--- ecommerce-server/routes/braintree.js
+++ ecommerce-server/routes/braintree.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { requireSignIn, isAuth } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
+const { generateToken } = require("../controllers/braintree");
 
 router.get("/braintree/getToken/:userId", requireSignIn, isAuth, generateToken);
 
